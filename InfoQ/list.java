a6980d4dd83a1f20386a89d49a022ff1fe17f909
Tom <tomjerrya@gmail.com>
2013-02-28 21:39:05 +0800

    second

---
+++ InfoQ/list.java
@@ -0,0 +1,35 @@
+<html>
+ <head></head>
+ <body>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/0.java">How Swarming Helps Agile Teams to Deliver</a>Swarming is a technique that helps agile teams to deliver working software fast and frequently. What is swarming, what are the benefits of swarming, and when and how to apply it?
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/1.java">Vagrant Support for Amazon AWS</a>HashiCorp's latest addition to support Amazon AWS as provider for Vagrant Virtual Machines (VM) is in a new league: for the first time it will be possible to spin up Virtual Machines in the cloud instead of the developer's desktop.
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/2.java">Vaadin 7 Arrives with GWT as an Integrated Component</a>Vaadin has released version 7 of its Java-based web application framework. The Vaadin 7 platform is the framework's first major update since 2009.
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/3.java">Concurrent Releases Lingual, a SQL DSL for Hadoop</a>Concurrent, Inc., the enterprise Big Data application platform company, today announced Lingual, an open source project enabling fast and simple Big Data application development on Apache Hadoop using SQL.
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/4.java">Applying Validation for Queryable API in ASP.NET Web API OData</a>In ASP.NET Web API OData, it is important to enable validation in Queryable API if you make use of queries outside the organization and this article examines the various scenarios where you can enable validation.
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/5.java">Greenplum Pivotal HD Combines the Strengths of SQL and Hadoop</a>EMC Greenplum has announced Pivotal HD, a new Hadoop distribution including a fully compliant SQL MPP database running on HDFS and being “hundreds of times faster than Hive”.
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/6.java">Using the Domain Driven Design Bounded Context Concept to Shrink a Large Domain Model</a>Bounded Context concept from Domain Driven Design (DDD) can be used to divide a large model into smaller models using the Database Context (DbContext class) in Entity Framework (EF). Bounded Context creates smaller, more cohesive models with boundaries between models.
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/7.java">SOLID Database Design With PostgreSQL</a>Chris Travers recently published a series of articles titled “Building SOLID Databases”. He explains several ideas on how to apply some of the common OOP principles such as Single Responsibility Principle, Interface Segregation, Dependency Inversion to improve data models and database code.
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/8.java">Oracle Updates NetBeans for HTML5</a>With the latest 7.3 release of NetBeans, Oracle has updated the IDE so developers can more easily build HTML5-based user interfaces for mobile and web applications, with code completion capabilities for HTML, JavaScript, and CSS3, and a new Nashorn-powered JavaScript editor. Netbeans also gains a new stand-alone JPQL editor that allows developers to test JPQL queries directly from the IDE.
+  </div>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/9.java">Brent Ozar on Improving Productivity with SQL Server</a>Brent Ozar, renowned SQL Server consultant recently shared few techniques for improving productivity with SQL Server.
+  </div>
+ </body>
+</html>
