629814f0b82a2eea34c602cf791a17404bd41775
Tom <tomjerrya@gmail.com>
2013-03-01 00:04:58 +0800

    update

--- InfoQ/3.java
+++ InfoQ/3.java
@@ -70,7 +70,7 @@
  <br /> 
  <br /> 
  <div style="text-align: center;">
-  <img src="/resource/news/2013/02/vaadin-7/en/resources/Vaadins_Development_and_Release_Process.jpg;jsessionid=F9ADA7EA2E83C28936A4F5AC5A8017FC" border="0" alt="Vaadin's Development and Release Process" _href="img://Vaadins_Development_and_Release_Process.jpg" _p="true" />
+  <img src="/resource/news/2013/02/vaadin-7/en/resources/Vaadins_Development_and_Release_Process.jpg;jsessionid=1DCB92212F64DEC5F805119408C5D2EF" border="0" alt="Vaadin's Development and Release Process" _href="img://Vaadins_Development_and_Release_Process.jpg" _p="true" />
  </div> 
  <div style="text-align: center; font-weight: bold;">
   Figure: Vaadin's Development and Release Process
