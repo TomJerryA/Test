62a808432d604725687301fd552639326fdc9f68
Tom <tomjerrya@gmail.com>
2013-03-01 23:20:45 +0800

    update

--- InfoQ/list.java
+++ InfoQ/list.java
@@ -1,35 +1,95 @@
 <html>
- <head></head>
- <body>
+ <head></head> 
+ <body> 
   <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/0.java">Casablanca's C++ SDK Whitelisted for Open Source</a>Microsoft's C++ REST SDK, codenamed Casablanca, has been open sourced under the Apache 2.0 license. Casablanca provides developers with a multiplatform way to write C++11 code that interacts with REST services.
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/1.java">Reactions to Yahoo!’s Decision to Forbid Employee Telecommuting</a>Yahoo!’s recent decision to call all employees to work from company’s offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!?
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/2.java">Eclipse RAP 2.0 Released - Same Acronym, Different Name</a>On January 11th, Eclipse RAP 2.0 was released, after 6 years of development. InfoQ talked to the project lead about the release.
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/3.java">How Swarming Helps Agile Teams to Deliver</a>Swarming is a technique that helps agile teams to deliver working software fast and frequently. What is swarming, what are the benefits of swarming, and when and how to apply it?
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/4.java">Vagrant Support for Amazon AWS</a>HashiCorp's latest addition to support Amazon AWS as provider for Vagrant Virtual Machines (VM) is in a new league: for the first time it will be possible to spin up Virtual Machines in the cloud instead of the developer's desktop.
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/5.java">Vaadin 7 Arrives with GWT as an Integrated Component</a>Vaadin has released version 7 of its Java-based web application framework. The Vaadin 7 platform is the framework's first major update since 2009.
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/6.java">Concurrent Releases Lingual, a SQL DSL for Hadoop</a>Concurrent, Inc., the enterprise Big Data application platform company, today announced Lingual, an open source project enabling fast and simple Big Data application development on Apache Hadoop using SQL.
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/7.java">Applying Validation for Queryable API in ASP.NET Web API OData</a>In ASP.NET Web API OData, it is important to enable validation in Queryable API if you make use of queries outside the organization and this article examines the various scenarios where you can enable validation.
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/8.java">Greenplum Pivotal HD Combines the Strengths of SQL and Hadoop</a>EMC Greenplum has announced Pivotal HD, a new Hadoop distribution including a fully compliant SQL MPP database running on HDFS and being “hundreds of times faster than Hive”.
-  </div>
-  <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/9.java">Using the Domain Driven Design Bounded Context Concept to Shrink a Large Domain Model</a>Bounded Context concept from Domain Driven Design (DDD) can be used to divide a large model into smaller models using the Database Context (DbContext class) in Entity Framework (EF). Bounded Context creates smaller, more cohesive models with boundaries between models.
-  </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/29.java">Casablanca's C++ SDK Whitelisted for Open Source</a>Microsoft's C++ REST SDK, codenamed Casablanca, has been open sourced under the Apache 2.0 license. Casablanca provides developers with a multiplatform way to write C++11 code that interacts with REST services.
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/28.java">Reactions to Yahoo!’s Decision to Forbid Employee Telecommuting</a>Yahoo!’s recent decision to call all employees to work from company’s offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!?
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/27.java">Eclipse RAP 2.0 Released - Same Acronym, Different Name</a>On January 11th, Eclipse RAP 2.0 was released, after 6 years of development. InfoQ talked to the project lead about the release.
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/26.java">How Swarming Helps Agile Teams to Deliver</a>Swarming is a technique that helps agile teams to deliver working software fast and frequently. What is swarming, what are the benefits of swarming, and when and how to apply it?
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/25.java">Vagrant Support for Amazon AWS</a>HashiCorp's latest addition to support Amazon AWS as provider for Vagrant Virtual Machines (VM) is in a new league: for the first time it will be possible to spin up Virtual Machines in the cloud instead of the developer's desktop.
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/24.java">Vaadin 7 Arrives with GWT as an Integrated Component</a>Vaadin has released version 7 of its Java-based web application framework. The Vaadin 7 platform is the framework's first major update since 2009.
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/23.java">Concurrent Releases Lingual, a SQL DSL for Hadoop</a>Concurrent, Inc., the enterprise Big Data application platform company, today announced Lingual, an open source project enabling fast and simple Big Data application development on Apache Hadoop using SQL.
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/22.java">Applying Validation for Queryable API in ASP.NET Web API OData</a>In ASP.NET Web API OData, it is important to enable validation in Queryable API if you make use of queries outside the organization and this article examines the various scenarios where you can enable validation.
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/21.java">Greenplum Pivotal HD Combines the Strengths of SQL and Hadoop</a>EMC Greenplum has announced Pivotal HD, a new Hadoop distribution including a fully compliant SQL MPP database running on HDFS and being “hundreds of times faster than Hive”.
+   </div>
+   <div>
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/20.java">Using the Domain Driven Design Bounded Context Concept to Shrink a Large Domain Model</a>Bounded Context concept from Domain Driven Design (DDD) can be used to divide a large model into smaller models using the Database Context (DbContext class) in Entity Framework (EF). Bounded Context creates smaller, more cohesive models with boundaries between models.
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/19.java">Casablanca's C++ SDK Whitelisted for Open Source</a>Microsoft's C++ REST SDK, codenamed Casablanca, has been open sourced under the Apache 2.0 license. Casablanca provides developers with a multiplatform way to write C++11 code that interacts with REST services. 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/18.java">Reactions to Yahoo!’s Decision to Forbid Employee Telecommuting</a>Yahoo!’s recent decision to call all employees to work from company’s offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!? 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/17.java">Eclipse RAP 2.0 Released - Same Acronym, Different Name</a>On January 11th, Eclipse RAP 2.0 was released, after 6 years of development. InfoQ talked to the project lead about the release. 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/16.java">How Swarming Helps Agile Teams to Deliver</a>Swarming is a technique that helps agile teams to deliver working software fast and frequently. What is swarming, what are the benefits of swarming, and when and how to apply it? 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/15.java">Vagrant Support for Amazon AWS</a>HashiCorp's latest addition to support Amazon AWS as provider for Vagrant Virtual Machines (VM) is in a new league: for the first time it will be possible to spin up Virtual Machines in the cloud instead of the developer's desktop. 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/14.java">Vaadin 7 Arrives with GWT as an Integrated Component</a>Vaadin has released version 7 of its Java-based web application framework. The Vaadin 7 platform is the framework's first major update since 2009. 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/13.java">Concurrent Releases Lingual, a SQL DSL for Hadoop</a>Concurrent, Inc., the enterprise Big Data application platform company, today announced Lingual, an open source project enabling fast and simple Big Data application development on Apache Hadoop using SQL. 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/12.java">Applying Validation for Queryable API in ASP.NET Web API OData</a>In ASP.NET Web API OData, it is important to enable validation in Queryable API if you make use of queries outside the organization and this article examines the various scenarios where you can enable validation. 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/11.java">Greenplum Pivotal HD Combines the Strengths of SQL and Hadoop</a>EMC Greenplum has announced Pivotal HD, a new Hadoop distribution including a fully compliant SQL MPP database running on HDFS and being “hundreds of times faster than Hive”. 
+   </div> 
+   <div> 
+    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/10.java">Using the Domain Driven Design Bounded Context Concept to Shrink a Large Domain Model</a>Bounded Context concept from Domain Driven Design (DDD) can be used to divide a large model into smaller models using the Database Context (DbContext class) in Entity Framework (EF). Bounded Context creates smaller, more cohesive models with boundaries between models. 
+   </div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/0.java">Casablanca's C++ SDK Whitelisted for Open Source</a>Microsoft's C++ REST SDK, codenamed Casablanca, has been open sourced under the Apache 2.0 license. Casablanca provides developers with a multiplatform way to write C++11 code that interacts with REST services. 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/1.java">Reactions to Yahoo!’s Decision to Forbid Employee Telecommuting</a>Yahoo!’s recent decision to call all employees to work from company’s offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!? 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/2.java">Eclipse RAP 2.0 Released - Same Acronym, Different Name</a>On January 11th, Eclipse RAP 2.0 was released, after 6 years of development. InfoQ talked to the project lead about the release. 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/3.java">How Swarming Helps Agile Teams to Deliver</a>Swarming is a technique that helps agile teams to deliver working software fast and frequently. What is swarming, what are the benefits of swarming, and when and how to apply it? 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/4.java">Vagrant Support for Amazon AWS</a>HashiCorp's latest addition to support Amazon AWS as provider for Vagrant Virtual Machines (VM) is in a new league: for the first time it will be possible to spin up Virtual Machines in the cloud instead of the developer's desktop. 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/5.java">Vaadin 7 Arrives with GWT as an Integrated Component</a>Vaadin has released version 7 of its Java-based web application framework. The Vaadin 7 platform is the framework's first major update since 2009. 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/6.java">Concurrent Releases Lingual, a SQL DSL for Hadoop</a>Concurrent, Inc., the enterprise Big Data application platform company, today announced Lingual, an open source project enabling fast and simple Big Data application development on Apache Hadoop using SQL. 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/7.java">Applying Validation for Queryable API in ASP.NET Web API OData</a>In ASP.NET Web API OData, it is important to enable validation in Queryable API if you make use of queries outside the organization and this article examines the various scenarios where you can enable validation. 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/8.java">Greenplum Pivotal HD Combines the Strengths of SQL and Hadoop</a>EMC Greenplum has announced Pivotal HD, a new Hadoop distribution including a fully compliant SQL MPP database running on HDFS and being “hundreds of times faster than Hive”. 
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/9.java">Using the Domain Driven Design Bounded Context Concept to Shrink a Large Domain Model</a>Bounded Context concept from Domain Driven Design (DDD) can be used to divide a large model into smaller models using the Database Context (DbContext class) in Entity Framework (EF). Bounded Context creates smaller, more cohesive models with boundaries between models. 
+  </div> 
  </body>
 </html>
