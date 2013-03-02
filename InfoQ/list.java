84dabdaef3b553b6789eeb046a7a7bb02619e694
Tom <tomjerrya@gmail.com>
2013-03-02 19:53:03 +0800

    update

--- InfoQ/list.java
+++ InfoQ/list.java
@@ -1,6 +1,9 @@
 <html>
  <head></head> 
- <body> 
+ <body>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/10.java">Reactions to Yahoo!â€™s Decision to Forbid Employee Telecommuting</a>Yahoo!â€™s recent decision to call all employees to work from companyâ€™s offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!?
+  </div> 
   <div> 
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/9.java">Entity Framework 6 Alpha 3 with Code First Stored Procedures and Connection Resiliency</a>Microsoft has released Entity Framework 6 Alpha 3 with support for code first stored procedures, connection resiliency, pull request from iceclow and UnaiZorrilla. 
   </div> 
@@ -8,7 +11,7 @@
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/8.java">Casablanca's C++ SDK Whitelisted for Open Source</a>Microsoft's C++ REST SDK, codenamed Casablanca, has been open sourced under the Apache 2.0 license. Casablanca provides developers with a multiplatform way to write C++11 code that interacts with REST services. 
   </div> 
   <div> 
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/7.java">Reactions to Yahoo!’s Decision to Forbid Employee Telecommuting</a>Yahoo!’s recent decision to call all employees to work from company’s offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!? 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/7.java">Reactions to Yahoo!ï¿½s Decision to Forbid Employee Telecommuting</a>Yahoo!ï¿½s recent decision to call all employees to work from companyï¿½s offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!? 
   </div> 
   <div> 
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/6.java">Eclipse RAP 2.0 Released - Same Acronym, Different Name</a>On January 11th, Eclipse RAP 2.0 was released, after 6 years of development. InfoQ talked to the project lead about the release. 
@@ -26,7 +29,7 @@
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/2.java">Applying Validation for Queryable API in ASP.NET Web API OData</a>In ASP.NET Web API OData, it is important to enable validation in Queryable API if you make use of queries outside the organization and this article examines the various scenarios where you can enable validation. 
   </div> 
   <div> 
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/1.java">Greenplum Pivotal HD Combines the Strengths of SQL and Hadoop</a>EMC Greenplum has announced Pivotal HD, a new Hadoop distribution including a fully compliant SQL MPP database running on HDFS and being “hundreds of times faster than Hive”. 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/1.java">Greenplum Pivotal HD Combines the Strengths of SQL and Hadoop</a>EMC Greenplum has announced Pivotal HD, a new Hadoop distribution including a fully compliant SQL MPP database running on HDFS and being ï¿½hundreds of times faster than Hiveï¿½. 
   </div> 
   <div> 
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/0.java">Using the Domain Driven Design Bounded Context Concept to Shrink a Large Domain Model</a>Bounded Context concept from Domain Driven Design (DDD) can be used to divide a large model into smaller models using the Database Context (DbContext class) in Entity Framework (EF). Bounded Context creates smaller, more cohesive models with boundaries between models. 
