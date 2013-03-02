05f9ff0b23d773ca5115a1c085c1838895d9b878
Tom <tomjerrya@gmail.com>
2013-03-02 13:02:25 +0800

    update

---
+++ InfoQ/7.java
@@ -0,0 +1,33 @@
+<p>Yahoo!’s recent decision to call all employees to work&nbsp;from company’s&nbsp;offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!?</p> 
+<p>A week ago, Jackie Reses, Executive VP of People and Development for Yahoo!, sent a <a href="http://allthingsd.com/20130222/physically-together-heres-the-internal-yahoo-no-work-from-home-memo-which-extends-beyond-remote-workers/#">memo</a> to all employees announcing them that all work-from-home arrangements are no longer valid and every Yahoo! employee needs to join the workforces in one of the offices available for them starting with June. Reses’ argument was that “communication and collaboration will be important, so we need to be working side-by-side” and she implied that Yahoo!’s telecommuters are not up to their job: “Speed and quality are often sacrificed when we work from home.”</p> 
+<p>Yahoo! has a large percentage of their staff working from home throughout all divisions: customer support, marketing, engineering, etc., and many are not productive, <a href="http://according%20to%20business%20insider%2C/">according to Business Insider</a>, as&nbsp;a former unnamed Yahoo! engineer told them: “For what it's worth, I support the no working from home rule. There's a ton of abuse of that at Yahoo. Something specific to the company.”</p> 
+<p>Michael Katz, a former ad tech executive for Yahoo!, also agrees with company’s decision:</p> 
+<blockquote> 
+ <p>The value in human interaction is greater collective wisdom as a result of improved communication &amp; collaboration.</p> 
+ <p>It's really all about improving the likelihood that meaningful interaction will translate to meaningful (shareholder) value.</p> 
+</blockquote> 
+<p>Business Insider also added that this move “upset lots of Yahoo employees – including some working mothers, who say they wish they could afford to build a nursery at the office the way CEO Marissa Mayer has.” Mayer had a baby last fall and she paid “<a href="http://www.businessinsider.com/marissa-mayer-who-just-banned-working-from-home-paid-to-have-a-nursery-built-at-her-office-2013-2">to have a nursery built in her office</a>.” “I wonder what would happen if my wife brought our kids and nanny to work and set them up in the cube next door?,” asked the husband of a remote Yahoo! worker.</p> 
+<p>A “source familiar with Mayer’s thought process” saw this announcement as an opportunity for Yahoo! to trim down the workforce being “a layoff that's not a layoff” and bringing the company in shape, currently having a “huge, bloated infrastructure” that “got fat and lazy over the past 15 years.”</p> 
+<p>Yahoo!’s approach to remote work has generated some reaction throughout the tech community. Scott Hanselman, a 5-years telecommuter veteran working for Microsoft, wrote on his blog that <a href="http://www.hanselman.com/blog/BeingARemoteWorkerSucksLongLiveTheRemoteWorker.aspx">he would quit if he were a Yahoo! employee</a>: “If I got this memo while working Remote at Yahoo I'd quit that moment. I would probably quit with some flair as well. Talk about completely demotivating.”</p> 
+<p>He sees three reasons why Yahoo! did it:</p> 
+<blockquote> 
+ <ul> 
+  <li>A veiled attempt to trim the workforce through effectively forced attrition by giving a Sophie's Choice to remote workers that management perceives as possibly not optimally contributing. It's easy to avoid calling it a layoff when you've just changed the remote work policy, right?</li> 
+  <li>A complete and total misstep and misunderstanding of how remote workers see themselves and how they provide value.</li> 
+  <li>Pretty clear evidence that Yahoo really has no decent way to measure of productivity and output of a worker.</li> 
+ </ul> 
+</blockquote> 
+<p>Hanselman believes that trust was lost inside Yahoo! somewhere along the way:</p> 
+<blockquote> 
+ <p>Ultimately, though, this comes down to trust, and trust can be found or lost on every page of a company's policies. You were hired as a professional; are you trusted to be a professional? Working remotely requires your company to trust you can do the work not only without them seeing you, but also without constant physical interaction with your teammates.</p> 
+</blockquote> 
+<p>Scott Berkun, an author and speaker, wrote that <a href="http://scottberkun.com/2013/in-defense-of-remote-work-and-marrisa-mayer/">Yahoo!’s decision to apply the same measure to everyone is a mistake</a> and it will actually backfire:</p> 
+<blockquote> 
+ <p>Any unilateral decision by an executive about how creative people work is a mistake. To presume to know what is best for hundreds of professional adults is to make yourself a parent, and make your employees children. The most talented employees who prefer autonomy will leave. The less talented and more dependent employees will stay.</p> 
+</blockquote> 
+<p>Berkun thinks that Yahoo! does not have data to back up the complaint that&nbsp;their employees sacrificed speed and quality by working at home, but he admits this “might be true at Yahoo.” He also adds that this move might help the company at the end:</p> 
+<blockquote> 
+ <p>She [Mayer] is the CEO and knows more about what’s going on in her company than we do. We’re on the outside looking in. A shock to the system might be precisely what Yahoo needs and targeting remote work was a specific way to get her message of “wake up and shape up” heard loudly.</p> 
+</blockquote> 
+<p>It is no secret that Yahoo! has lost its edge and it is not a web front runner anymore, and they have to do something about it. Some people who prefer or need to be working from home will leave the company. Others will go to the office every day from June. If this move is an attempt to cut the number of employees, then it is not a good one because the company trims personnel without discrimination, losing the good along&nbsp;with the bad ones.&nbsp;If this is a rebuilt of the company and its culture, then we have to wait a couple of years to see where it is heading to. Anyway, this is a strong signal regarding remote working and some managers will think twice before letting their employees working from home.</p> 
+<p id="lastElm"></p>
