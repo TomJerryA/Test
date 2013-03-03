63175c119faee3944a67d2e367f47b9fa27407ed
Tom <tomjerrya@gmail.com>
2013-03-04 00:59:54 +0800

    update

--- InfoQ/10.java
+++ InfoQ/10.java
@@ -1,33 +1,58 @@
-<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body><h3>Reactions to Yahoo!’s Decision to Forbid Employee Telecommuting</h3><p>Yahoo!’s recent decision to call all employees to work&nbsp;from company’s&nbsp;offices has raised some questions especially throughout the telecommuting community. Is this a disguised move to lay off people? Is it a misunderstanding of how remoting is to be managed? Is this the right move for Yahoo!?</p> 
-<p>A week ago, Jackie Reses, Executive VP of People and Development for Yahoo!, sent a <a href="http://allthingsd.com/20130222/physically-together-heres-the-internal-yahoo-no-work-from-home-memo-which-extends-beyond-remote-workers/#">memo</a> to all employees announcing them that all work-from-home arrangements are no longer valid and every Yahoo! employee needs to join the workforces in one of the offices available for them starting with June. Reses’ argument was that “communication and collaboration will be important, so we need to be working side-by-side” and she implied that Yahoo!’s telecommuters are not up to their job: “Speed and quality are often sacrificed when we work from home.”</p> 
-<p>Yahoo! has a large percentage of their staff working from home throughout all divisions: customer support, marketing, engineering, etc., and many are not productive, <a href="http://according%20to%20business%20insider%2C/">according to Business Insider</a>, as&nbsp;a former unnamed Yahoo! engineer told them: “For what it's worth, I support the no working from home rule. There's a ton of abuse of that at Yahoo. Something specific to the company.”</p> 
-<p>Michael Katz, a former ad tech executive for Yahoo!, also agrees with company’s decision:</p> 
-<blockquote> 
- <p>The value in human interaction is greater collective wisdom as a result of improved communication &amp; collaboration.</p> 
- <p>It's really all about improving the likelihood that meaningful interaction will translate to meaningful (shareholder) value.</p> 
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body><h3>Windows Blue with New Kernel, Internet Explorer 11, Bing Integration, Updated Apps</h3><p>It all began with a job posting on the <a href="https://careers.microsoft.com/jobdetails.aspx?ss=&amp;pg=0&amp;so=&amp;rw=14&amp;jid=104227&amp;jlang=EN&amp;pp=SS">Microsoft</a> official careers page which no longer exist as of the time of this writing. Microsoft has been on a lookout for an experienced engineer to join the Core Experience team in Windows Sustained Engineering (WinSE). The listing also mentioned the following:</p> 
+<blockquote>
+  Windows Blue promises to build and improve upon these aspects of the OS, enhancing ease of use and the overall user experience on devices and PCs worldwide. 
 </blockquote> 
-<p>Business Insider also added that this move “upset lots of Yahoo employees – including some working mothers, who say they wish they could afford to build a nursery at the office the way CEO Marissa Mayer has.” Mayer had a baby last fall and she paid “<a href="http://www.businessinsider.com/marissa-mayer-who-just-banned-working-from-home-paid-to-have-a-nursery-built-at-her-office-2013-2">to have a nursery built in her office</a>.” “I wonder what would happen if my wife brought our kids and nanny to work and set them up in the cube next door?,” asked the husband of a remote Yahoo! worker.</p> 
-<p>A “source familiar with Mayer’s thought process” saw this announcement as an opportunity for Yahoo! to trim down the workforce being “a layoff that's not a layoff” and bringing the company in shape, currently having a “huge, bloated infrastructure” that “got fat and lazy over the past 15 years.”</p> 
-<p>Yahoo!’s approach to remote work has generated some reaction throughout the tech community. Scott Hanselman, a 5-years telecommuter veteran working for Microsoft, wrote on his blog that <a href="http://www.hanselman.com/blog/BeingARemoteWorkerSucksLongLiveTheRemoteWorker.aspx">he would quit if he were a Yahoo! employee</a>: “If I got this memo while working Remote at Yahoo I'd quit that moment. I would probably quit with some flair as well. Talk about completely demotivating.”</p> 
-<p>He sees three reasons why Yahoo! did it:</p> 
-<blockquote> 
- <ul> 
-  <li>A veiled attempt to trim the workforce through effectively forced attrition by giving a Sophie's Choice to remote workers that management perceives as possibly not optimally contributing. It's easy to avoid calling it a layoff when you've just changed the remote work policy, right?</li> 
-  <li>A complete and total misstep and misunderstanding of how remote workers see themselves and how they provide value.</li> 
-  <li>Pretty clear evidence that Yahoo really has no decent way to measure of productivity and output of a worker.</li> 
- </ul> 
+<p>According to reports coming in, the next major update to Windows 8 ecosystem codenamed Windows Blue will be available for consumers towards the middle of this year. <br /> <br /> When contacted, Microsoft refused to disclose any information related to Windows Blue. InfoQ quizzed Microsoft with several questions and was able to get only the following reply</p> 
+<blockquote>
+  We do not comment on rumors and speculation 
 </blockquote> 
-<p>Hanselman believes that trust was lost inside Yahoo! somewhere along the way:</p> 
-<blockquote> 
- <p>Ultimately, though, this comes down to trust, and trust can be found or lost on every page of a company's policies. You were hired as a professional; are you trusted to be a professional? Working remotely requires your company to trust you can do the work not only without them seeing you, but also without constant physical interaction with your teammates.</p> 
+<p>According to a news story reported by Win8China.com, a public preview of Windows Blue is expected in June with the actual release scheduled for August. The site also claims that Windows Blue will be built on a new kernel with Internet Explorer 11.</p> 
+<p>The big rumor spreading around the web is that Windows Blue will ship with <a href="http://www.bing.com">Bing</a> search engine fully integrated into it. However, federal regulators will keep a close watch as it will possibly affect the consumer's freedom in choosing their desired search engine.<br /> <br /> According to reports, Windows Blue will provide support for small tablet screen sizes such as 7 and 8 inch displays. Microsoft will probably release a mini version of Surface tablet by the end of this year including a possibility of a Surface phone with Windows Phone 8.</p> 
+<p>Microsoft will release Windows Blue as a free add-on to Windows 8 just like updates and service pack to its previous counterparts. However, industry experts feel that this will put pressure on Apple as they charge around $20 for its software updates.<br /> <br /> &quot;Windows Blue could have a profound impact on the marketplace especially if it helps Microsoft finally get Windows 8 into more offices and homes,&quot; said Don Reisinger, eweek.com<br /> <br /> According to Don, Windows 8 isn't appealing to consumers.</p> 
+<p>Different people will have different taste and preferences. Some people like it while some doesn't. It's purely a personal preference which we can't judge completely unless a comprehensive sample survey is conducted.</p> 
+<p>Consumers who attempt to try Windows 8 should have used previous versions of Windows which has the traditional Start button on the TaskBar. Windows 8 doesn't include Start button. It remains to be seem whether Windows Blue will ship with an option to enable the Start menu although third party tools are currently available.<br /> <br /> Don is of the opinion that corporate consumers will not like user interface which ships with existing Windows 8 and blue attempts to address that problem with some improvements that will appeal to enterprise users.<br /> <br /> &quot;Windows Blue will have better upgraded apps and will tweak the user experience by eliminating all of the extras in the interface that they don't need,&quot; mentions Don in his blog post</p> 
+<p>Windows 8 generated publicity when it was released last year but according to reports on the web some consumers are still happy with Windows 7. With the release of Windows Blue, consumers will have renewed interest to test drive Windows 8 to see what the new update has to offer.</p> 
+<p>Microsoft is really fighting hard in its quest to compete with Apple and Google in both desktop and mobile technology space. Windows 8 PCs are not able to attain significant boost in sales due to complete change in its user interface which some people doesn't like it very much. Microsoft has realized this situation and hence working on an update which increases the usability and productivity of consumers while working with Windows 8.<br /> <br /> &quot;Windows Blue will follow the Office 365 subscription model. When your subscription is up, you are locked out from all internet access except to pay by credit card or Paypal at a Microsoft web site,&quot; said Ben Myers.<br /> <br /> Matous Vales</p> 
+<blockquote>
+  Blue was the very much needed update to fix Windows 8 on non touch devices. Not only Windows 8 desktop inteface looks awful combined with Metro, navigating through Metro with a mouse is a total nightmare 
+ <br /> 
+ <br /> I have 2 ordinary displays and the experience sucks. I wanted to improve it with like a Microsoft touch mouse, but then I found out it has only a single button, so that’s a no-go. The software might be great on touchscreens 
 </blockquote> 
-<p>Scott Berkun, an author and speaker, wrote that <a href="http://scottberkun.com/2013/in-defense-of-remote-work-and-marrisa-mayer/">Yahoo!’s decision to apply the same measure to everyone is a mistake</a> and it will actually backfire:</p> 
-<blockquote> 
- <p>Any unilateral decision by an executive about how creative people work is a mistake. To presume to know what is best for hundreds of professional adults is to make yourself a parent, and make your employees children. The most talented employees who prefer autonomy will leave. The less talented and more dependent employees will stay.</p> 
+<p>pascal.vermeulen</p> 
+<blockquote>
+ I am working with Win8 Pro on my Workstation with 4 displays + 1 touchscreen, works like a charm 
+ <p>Win8 is 2 times (that is 200%) faster. The laptop that I am using to write this reply is 4 years old and boots up in 6 seconds (with a 60gig intel SSD, I must confess). But Win8 is really really fast, no one can deny that.</p> 
 </blockquote> 
-<p>Berkun thinks that Yahoo! does not have data to back up the complaint that&nbsp;their employees sacrificed speed and quality by working at home, but he admits this “might be true at Yahoo.” He also adds that this move might help the company at the end:</p> 
-<blockquote> 
- <p>She [Mayer] is the CEO and knows more about what’s going on in her company than we do. We’re on the outside looking in. A shock to the system might be precisely what Yahoo needs and targeting remote work was a specific way to get her message of “wake up and shape up” heard loudly.</p> 
+<p>MeesBoe</p> 
+<blockquote>
+  Windows 8 is a serious beauty 
 </blockquote> 
-<p>It is no secret that Yahoo! has lost its edge and it is not a web front runner anymore, and they have to do something about it. Some people who prefer or need to be working from home will leave the company. Others will go to the office every day from June. If this move is an attempt to cut the number of employees, then it is not a good one because the company trims personnel without discrimination, losing the good along&nbsp;with the bad ones.&nbsp;If this is a rebuilt of the company and its culture, then we have to wait a couple of years to see where it is heading to. Anyway, this is a strong signal regarding remote working and some managers will think twice before letting their employees working from home.</p> 
+<p>bartw78</p> 
+<blockquote>
+ I prefer Bing over Google even though I am in one of those countries outside the US. 
+</blockquote> 
+<p>Crumbleshake</p> 
+<blockquote>
+ I've been using Windows 8 since the release preview on my main PC with two monitors, no touchscreens, a mouse and a keyboard. I use metro a lot and I use the desktop a lot. It's absolute fiction that it's not good for a mouse and keyboard. 
+</blockquote> 
+<p>Arcadium</p> 
+<blockquote>
+  Metro with a mouse is totally usable, it just doesn’t seem to be your cup of tea. 
+</blockquote> 
+<p>abdhoms</p> 
+<blockquote>
+  If they bring features worth buying, I'll be ready to pay. 
+</blockquote> 
+<p><a href="http://winsupersite.com/windows-8/feeling-blue-microsoft-you-re-releasing-it-wrong">Paul Thurrott</a>, a renowned Windows expert is very critical about the upcoming release of Windows Blue:</p> 
+<blockquote>
+  Why is Microsoft working on a major update to Windows 8 when so many small things need to be fixed first, and faster? It requires fixing those horrible, terribly incomplete Metro apps one by one. Add a unified inbox view to the Mail app. Fix Xbox Music and get the matching service up and running. Add CalDAV support to Calendar. And so on.
+ <br /> 
+ <br /> Release an update that lets us skip the Start screen and boot right to the desktop and then make that a formal PC Settings option in Windows 9. 
+</blockquote> 
+<p>Consumers who purchased laptops with Windows 8 are not happy as rahvint on <a href="https://forums.lenovo.com/t5/IdeaPad-Y-U-V-Z-and-P-series/Issues-with-Y500/td-p/944841">Lenovo</a> forum commented:</p> 
+<blockquote>
+  This was my first experience with Windows 8 and it is just complete junk IMHO, I'm embarrassed to admit it took me longer than a minute to figure out how to restart my machine. 
+</blockquote> 
+<p>Based on the above statement, a fresh update to Windows 8 is a necessity to address specific usability issues.</p> 
+<p><em>The views expressed are the personal opinion of the author and not that of InfoQ. The quotes extracted from other sources doesn't reflect the opinion of the either the author or InfoQ.</em></p> 
 <p id="lastElm"></p></body></html>
