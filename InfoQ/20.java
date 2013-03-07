87b45a47f729812aad3004e833a5fd3228159944
Tom <tomjerrya@gmail.com>
2013-03-07 21:34:19 +0800

    update

---
+++ InfoQ/20.java
@@ -0,0 +1,35 @@
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body><h3>How can Agile make you Faster?</h3><p>A reason that enterprises mention why&nbsp;want to use agile for software development is that they want&nbsp;to deliver faster. There are studies that show that agile projects&nbsp;can be&nbsp;faster, like the Columbus Agile Productivity Benchmark Project described in <a href="http://www.infoq.com/news/2012/11/success-agile-projects;jsessionid=9C5397F8FB554AADCDFFC081F34108DA">evidence of success of agile projects</a>. How can agile&nbsp;be used&nbsp;to become&nbsp;faster?</p> 
+<p>In the blog post <a href="http://news.idg.no/cw/art.cfm?id=1FFBF5B6-0806-32B0-91FADBB886D47464">who says agile development can't be faster?</a>, Matthew Heusser shared a discussion that he had at the agile testing days conference:</p> 
+<blockquote> 
+ <p>At Agile Testing Days in Potsdam, Germany, in November 2012, Lisa Crispin and Janet Gregory, authors of Agile Testing: A Practical Guide , made the bold claim that &quot;agile means faster&quot; is a myth.</p> 
+</blockquote> 
+<p>Later in the conference, Janet Gregory explained to Matthew Heusser what she meant with this:</p> 
+<blockquote> 
+ <p>The point of agile is not speed, she says. It might be a by-product, but it won't happen right this second. (…) An agile conversion slows you down, at least in the short term-and that short term is not a week or two. It may be a year or two.</p> 
+</blockquote> 
+<p>Matthew provides several arguments why he considers agile to be faster. He explains how building the right things and skipping requirements that are not worth doing saves time. Another reason to use agile is that “The Old Way Isn't Fast, Either<strong>”:</strong></p> 
+<blockquote> 
+ <p>(…) comparing an agile team that can't get something done in a year to a more traditional team that can is a false comparison. In a year, the traditional team will have 12-and-a-half requirements done, a mess on the floor and nothing to ship.</p> 
+</blockquote> 
+<p>He concludes his blog post by explaining why he disagrees, and explains how he thinks that agile can help teams to become faster:</p> 
+<blockquote> 
+ <p>One question remains: Is it faster? Crispin and Gregory might argue that it does not matter, that focusing on pure speed in the short term leads to shortcuts, pain and slow performance in the long term. I contend that teams can focus on eliminating waste and improve velocity as they improve process.</p> 
+</blockquote> 
+<p>In <a href="http://skipoleschris.blogspot.nl/2012/07/making-agile-go-fast.html">making agile go fast</a>, Chris Turner&nbsp;reflects on reasons why agile project can go slow. He describes four reasons that he often sees, and gives some suggestions how to handle them:</p> 
+<ul> 
+ <li>The wrong people: Remove people from the team&nbsp;who aren’t following good engineering discipline or are&nbsp;making things more complex.</li> 
+ <li>Putting the process first: Establish open&nbsp;communication and self organising and empowered teams.</li> 
+ <li>Using the wrong technologies: give teams authority to make technology decisions, and allow technology choices to be reversed if they hinder delivery.</li> 
+ <li>Making the architecture too complex: Keep your software&nbsp;as simple as possible, refactor.&nbsp;</li> 
+</ul> 
+<p>Neil Killick describes in his blog post <a href="http://neilkillick.com/2012/10/24/sustainable-pace-the-fastest-way-to-deliver-software/">sustainable pace – the Fastest way to deliver software</a>&nbsp;how asking an agile team to speed up can slow down software development. He tells a story about&nbsp;an agile&nbsp;team that is capable of delivering&nbsp;on average 10 stories in every 2-week Sprint, where the number of stories to be delivered is increased:</p> 
+<blockquote> 
+ <p>Now imagine we asked the team to take on just ONE story every Sprint. (…) Well, we can’t be sure but it is fairly safe to assume that the 1 story is guaranteed to be delivered. We can also be pretty sure that it will be of an extremely high quality</p> 
+ <p>(…) we ask the team to deliver 2 stories per Sprint. While it’s highly likely that the team will surely deliver the 2 stories, the probability is slightly less than when we asked them to deliver 1 story. So we have a little less predictability.</p> 
+ <p>Now imagine we’re struggling to hit a contractual deadline so we feel the need to “speed up”. So we ask the team that predictably delivers 10 stories to deliver 12 (now we’re over capacity). Or even 14? (…) the “faster” we ask (or worse, tell) our teams to go, the&nbsp;less predictable at delivering software we become, and that software is more likely to be of a lower quality.</p> 
+</blockquote> 
+<p>He advices to allow teams to work at a sustainable pace:</p> 
+<blockquote> 
+ <p>(…) by allowing the team to find the right balance and deliver high quality software at their capacity, a cycle of success is created.</p> 
+</blockquote> 
+<p id="lastElm"></p><br><br><br><br><br><br></body></html>
