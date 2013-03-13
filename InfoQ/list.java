e868dbfccdeb2e31376fe902634e13df753e3ffc
Tom <tomjerrya@gmail.com>
2013-03-14 04:17:36 +0800

    update

--- InfoQ/list.java
+++ InfoQ/list.java
@@ -2,7 +2,10 @@
  <head></head> 
  <body>
   <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/43.java">Tom Baeyens, creator of jBPM and Alfresco Activiti, Starts New Venture to Bring BPM to the Cloud</a>Tom Baeyens, the original creator of JBoss jBPM (acquired by Red Hat) and Activiti (acquired by Alfresco), has announced Effektif, a new cloud-based Business Process Management (BPM) tool for process automation. The new venture is being developed in partnership with Signavio. InfoQ spoke to Baeyens and co-CEO of Signavio Decker to find out more.
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/44.java">Node.js 0.10 Released</a>The Node.js development team has released version 0.10 of node.js, the Javascript-based platform for building high-performance asynchronous servers. The release features an easier to use module for handling streams, better error handling with domains, and performance improvements. The team also announced that after version 0.10, one more stable release (0.12) will follow before its 1.0 release.
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/43.java">Tom Baeyens, creator of jBPM and Alfresco Activiti, Starts New Venture to Bring BPM to the Cloud</a>Tom Baeyens, the original creator of JBoss jBPM (acquired by Red Hat) and Activiti (acquired by Alfresco), has announced Effektif, a new cloud-based Business Process Management (BPM) tool for process automation. The new venture is being developed in partnership with Signavio. InfoQ spoke to Baeyens and co-CEO of Signavio Decker to find out more. 
   </div> 
   <div> 
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/42.java">Microsoft Open Sources Kinect for Windows Samples</a>Microsoft has recently open sourced on CodePlex 22 code samples under the Apache 2.0 license. The samples which are also included in the Kinect for Windows Toolkit show how to make use of various Kinect features: Audio, Basic Interactions, Colors, Depth, Face Tracing, Infrared, Slideshow Gestures, Speech, WPF, XNA, and others. 
