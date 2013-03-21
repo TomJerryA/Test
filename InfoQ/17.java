207a11a9bc0fd21ce03446c6c7e7f5fb5a28650d
Tom <tomjerrya@gmail.com>
2013-03-21 21:37:10 +0800

    update

--- InfoQ/17.java
+++ InfoQ/17.java
@@ -1,8 +1,59 @@
-<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body><h3>Final Preview of Visual Studio 2012 Update 2 Released</h3><p class="western" style="margin-bottom: 0in"><span lang="en-US">Continuing the progression of releasing regular previews prior to the official launch of Update 2, Microsoft announced the release of <a class="western" href="http://www.microsoft.com/en-us/download/details.aspx?id=36833">CTP4</a>. This release marks the first version of Update 2 that is </span><span lang="en-US">designated</span><span lang="en-US"> as &quot;go-live&quot;, meaning that it will receive full Microsoft support </span><span lang="en-US">(</span><span lang="en-US"><b>note</b></span><span lang="en-US"> differences between TFS2012 go-live and VS2012 go-live below)</span><span lang="en-US">. </span><span lang="en-US">The is intended to be the last CTP before the formal release of Update 2.</span></p> 
-<p class="western" style="margin-bottom: 0in"><span lang="en-US">CTP4 continues promoting the overall theme of Update 2 – &quot;Modern Application Lifecyle and Modern App Development&quot;. To serve that end, the first of the features released is an updated preview of the <a class="western" href="http://blogs.msdn.com/b/lightswitch/archive/2013/03/04/announcing-a-new-preview-of-the-lightswitch-html-client.aspx">LightSwitch HTML Client</a>. John Stallo, Lead Program Manager, comments that the two main goals of this release are to support the creation of mobile, cross-platform web clients and the publishing of programs to the SharePoint 2013 app catalog.</span></p> 
-<p class="western" style="margin-bottom: 0in">The browser-based test management area of VS2012 receives further refinement with the ability to edit text for results and test case steps inline, without requiring software to be installed in the environment being tested.</p> 
-<p class="western" style="margin-bottom: 0in"><span lang="en-US">Microsoft's Brian </span><span lang="en-US">Harry also noted that the back-up &amp; restore power tool (formerly a separate <a class="western" href="http://visualstudiogallery.msdn.microsoft.com/b1ef7eb2-e084-4cb8-9bc7-06c3bad9148f">download</a>) will be integrated into Update 2. TFS administrators should also benefit from the ability to preserve their <a class="western" href="http://blogs.msdn.com/b/visualstudioalm/archive/2013/03/05/tfs-update-2-offers-settings-preservation-for-in-place-upgrades.aspx">TFS configuration </a></span><a class="western" href="http://blogs.msdn.com/b/visualstudioalm/archive/2013/03/05/tfs-update-2-offers-settings-preservation-for-in-place-upgrades.aspx"><span lang="en-US">in-place</span></a><span lang="en-US"> after an</span><span lang="en-US"> upgrade.</span></p> 
-<p class="western" style="margin-bottom: 0in"><span lang="en-US">Developers whom require </span><span lang="en-US"><a class="western" href="http://www.infoq.com/news/2013/01/vs2012_git;jsessionid=DFF196405AD2D49DCE4BBE067DED1492">Git support</a> may want to wait before installing CTP4 a</span><span lang="en-US">s a separate, yet-to-be-released, update to the <a class="western" href="http://visualstudiogallery.msdn.microsoft.com/abafc7d6-dcaa-40f4-8a5e-d6724bdb980c">Git extension</a> is needed to provide this functionality. Harry states that this will be available &quot;within the next day or two&quot;. This support is not part of Update 2, and will continue to require a separate download as Harry notes that this allow Microsoft to more rapidly evolve the offering.</span></p> 
-<p class="western" style="margin-bottom: 0in">Note that the go-live status of CTP4 applies to Team Foundation Server 2012 <u>exclusively</u>. This is not true of CTP4 for VS2012, and as a result CTP4 for VS2012 should not be applied to production environments.</p> 
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body><h3>ZeroTurnaround has Announced the Availability of LiveRebel 2.6</h3><p>ZeroTurnaround has announced the availability of LiveRebel 2.6, its software release automation tool.</p> 
+<p>Version 2.6 adds the following features:</p> 
+<ul> 
+ <li>Support for applications built using Java, PHP, Python, PERL and Ruby.</li> 
+ <li>Database updates and version control, giving operations teams the ability to update or rollback database changes along with associated applications.</li> 
+ <li>Multiplatform updates, enabling teams to release applications built using multiple platforms and databases across many unlike environments.</li> 
+ <li>Property management that allows deploying a single release onto several, unlike environments by applying specific configurations for each one behind the scenes.</li> 
+ <li>Monitors the health of the application and the server it runs on in real-time.</li> 
+</ul> 
+<p>InfoQ has interviewed Krishnan Badrinarayanan ZeroTurnaround Product Marketing Manager for LiveRebel.</p> 
+<p><b>I see you position LiveRebel as a test and release tool, but you are not a continuous integration tool. What does LiveRebel add to the test and release equation?</b></p> 
+<blockquote> 
+ <p>When you think about releasing apps, CI tools can be thought of as scripting on steroids. You can do anything with them, but you have to code and maintain it all. They do not have knowledge about your environment, configuration, database or anything else. They just trigger and execute tasks one after another and log success or failure.</p> 
+ <p>LiveRebel approaches this differently. It knows everything about your environment - which servers are running, and which apps and versions are deployed. It manages and versions properties specific to environments. It understands and versions database and environment changes.</p> 
+ <p>A typical use case: An agile development team releases updates every few days. The QA team picks each release up, and using LiveRebel automatically deploys it - code, DB, conf - onto their test environments. Once deployed, LiveRebel automatically calls test scripts. Once passed, the Operations team deploys the app onto the staging environment. LiveRebel automatically calls smoke tests. Finally, the operations team deploys the app onto the production environment with no downtime. If any deployment were to fail, LiveRebel automatically rolls back changes.</p> 
+ <p>In the end LiveRebel releases apps - code, DB and config, all in-sync, across physical, virtual or cloud environments. Deployments are versioned, automated, fully reversible and testable. The result: teams better manage and expedite releases predictably, without compromising quality or disrupting user experience.</p> 
+</blockquote> 
+<p><b>Is a CI tool actually required or can LiveRebel perform CI functions as well?</b></p> 
+<blockquote> 
+ <p>They occupy different niches. LiveRebel doesn't do builds, it comes after the builds were done. LiveRebel is a release automation tool that also integrates with popular CI tools. For example, if the development team were to package a release by hand, they can use the LiveRebel command center to upload it, and then deploy it onto various environments, quickly and safely.</p> 
+ <p>However, if the team were to use a CI tool like Jenkins, Hudson or Bamboo, they can install the LiveRebel plugin and create deployment tasks that would hand off deployment to LiveRebel. LiveRebel would then deploy the release - code, DB and conf, all together - onto selected environments with no downtime and in a failsafe manner.</p> 
+</blockquote> 
+<p><b> </b></p> 
+<p><b>Can you talk about property management?</b><b> </b></p> 
+<blockquote> 
+ <p>When you release apps through testing, staging, production, or onto customer or external environments, you encounter differences in environment properties, environment variables and configuration. Often this is solved by packaging a separate release for each environment. This means that your releases are subtly different between environments and cannot be tested completely. Others have found a way to externalize these environment specific properties using home-grown scripts and tools.</p> 
+ <p>With LiveRebel, release engineers can specify property sets that apply for each environment. LiveRebel then deploys the same package to all chosen environments and applies their respective property sets. It will even alert you if you are missing some properties.</p> 
+ <p>This means that you can run the same release process through testing, staging and production and be sure it's tested before getting to users.</p> 
+</blockquote> 
+<p><b>What about the monitoring capabilities?</b></p> 
+<blockquote> 
+ <p>LiveRebel provides basic application and server monitoring by placing a beacon beside each app or server. The beacons pulsate based on the number of requests they receive, and turns from green to red based on their health. On mouse-over, the beacon provides a pop up with key stats on response throughput. It's not a replacement for a full-blown Application Performance Monitor, but it provides the key metrics to monitor the server and application health.</p> 
+</blockquote> 
+<p><b> </b></p> 
+<p><b>Do you support the usual build tools, Ant, Maven, Gradle?</b><b> </b></p> 
+<blockquote> 
+ <p>Yes. LiveRebel offers a fully featured command line interface and REST API. So, with a couple lines of scripting, engineers can integrate LiveRebel with build tools.</p> 
+</blockquote> 
+<p><b>Do you integrate with Hudson, Bamboo, and TeamCity, and the various source control systems?</b></p> 
+<blockquote> 
+ <p>LiveRebel has ready-made plugins for Hudson, Jenkins and Bamboo. A plugin in for TeamCity is in the works. The plugins can be installed from each of their plugins menu and set up to communicate with LiveRebel securely. Once set up, you can automatically deploy releasable artifacts right from the CI tool via LiveRebel into selected environments with no downtime. All deployments are failsafe, which means if a failure were to occur, LiveRebel automatically rolls back any changes before users are impacted. Any source control system will do. For managing app configuration LiveRebel uses GIT.</p> 
+</blockquote> 
+<p><b>This seems like a completely different tool than sister product JRebel. Is there any kind of synergy?</b></p> 
+<blockquote> 
+ <p>Absolutely. It's a part of our mission to help software eat the world faster! We help software teams make the process of developing and delivering apps more pleasant and productive.</p> 
+</blockquote> 
+<p><b>What operating systems, Windows, Unix, Linux?</b></p> 
+<blockquote> 
+ <p>All of the above and the MacOS X. <a href="http://zeroturnaround.com/software/liverebel/what-we-support/#headline">Here is a complete list</a> of what we support.</p> 
+</blockquote> 
+<p><b> </b></p> 
+<p><b>Your website says this is free. What is your pricing model?</b><b> </b></p> 
+<blockquote> 
+ <p>LiveRebel is free for up to 2 managed servers. Which means you can deploy apps on up to two servers that host apps for free. Database servers do not count towards this quota. Any additional managed servers can be acquired at $420 per year.</p> 
+</blockquote> 
+<p><b> </b></p> 
+<p>Badrinarayanan told InfoQ that the next steps in product development will focus on increasing platform coverage and improving release management support. More information on LiveRebel is available <a href="http://www.liverebel.com">here</a>.</p> 
 <p>&nbsp;</p> 
 <p id="lastElm"></p><br><br><br><br><br><br></body></html>
--- InfoQ/list.java
+++ InfoQ/list.java
@@ -2,7 +2,10 @@
  <head></head> 
  <body>
   <div>
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/16.java">Develop Utility, Productivity, Financial and Media Apps with RadControls for Windows 8</a>Telerik has recently released RadControls for Windows 8, which enables developers to build touch optimized applications for desktop and mobile devices.
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/17.java">ZeroTurnaround has Announced the Availability of LiveRebel 2.6</a>ZeroTurnaround has announced the availability of LiveRebel 2.6, its software release automation tool. InfoQ has interviewed Krishnan Badrinarayanan Product Manager to learn more about LiveRebel and the latest release.
+  </div> 
+  <div> 
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/16.java">Develop Utility, Productivity, Financial and Media Apps with RadControls for Windows 8</a>Telerik has recently released RadControls for Windows 8, which enables developers to build touch optimized applications for desktop and mobile devices. 
   </div> 
   <div> 
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/15.java">Should You Create User Stories for Technical Debt?</a>Agile teams sometimes struggle with the planning of pure technical tasks that have no direct value for the user of a system, but have to be done to deliver working software. Should you create user stories to handle such technical tasks and technical debt, or not? 
@@ -147,9 +150,6 @@
   </div> 
   <div> 
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/18.java">Will Java 8 solve PermGen OutOfMemoryError?</a>As part of Oracle's ongoing project to merge the HotSpot and JRockit codebases, Oracle has announced that they will remove PermGen from the Java 8 version of the HotSpot JVM. The effects of the removal of PermGen can now be checked with Java 8 Early Access builds, it is time to find out if all PermGen problems have been resolved. 
-  </div> 
-  <div> 
-   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/17.java">Final Preview of Visual Studio 2012 Update 2 Released</a>The anticipated final preview of VS2012/TFS2012 Update 2 has been released. It brings a few more updates including the highlighting of the new LightSwitch HTML client. 
   </div>  
  </body>
 </html>
