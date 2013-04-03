c6d06c7a97e7751b97004e82ca942ce9f0dac644
Tom <tomjerrya@gmail.com>
2013-04-04 04:25:47 +0800

    update

---
+++ InfoQ/34.c
@@ -0,0 +1,59 @@
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body><h3>Melhoria de processos de negócios usando Agile: discussões recentes</h3><p>Organiza&ccedil;&otilde;es querem melhorar seus processos de neg&oacute;cios e nos dias de hoje precisam fazer essa melhoria de forma r&aacute;pida. &Eacute; poss&iacute;vel utilizar t&eacute;cnicas &aacute;geis para melhoria de processos de neg&oacute;cios?</p> 
+<p>No artigo <a href="http://blogs.hbr.org/cs/2012/10/when_it_comes_to_operational.html">A pr&oacute;xima onda da estrat&eacute;gia de neg&oacute;cios</a>, publicado pela Harvard Business Review, Brad Power compartilha sua vis&atilde;o sobre as abordagens de melhoria de processos de neg&oacute;cio. Apesar da melhoria alcan&ccedil;ada nas &uacute;ltimas d&eacute;cadas, Power percebe uma necessidade cont&iacute;nua das organiza&ccedil;&otilde;es de melhorar seus processos:</p> 
+<blockquote> 
+ <p>n&atilde;o &eacute; o momento de celebrar as vit&oacute;rias de ontem. Essas vit&oacute;rias n&atilde;o ir&atilde;o imunizar sua organiza&ccedil;&atilde;o contra o progresso cont&iacute;nuo desta d&eacute;cada. Isso porque a tecnologia da informa&ccedil;&atilde;o ... tornar&aacute; at&eacute; mesmo alguns dos processos de neg&oacute;cios atuais mais proficientes, ultrapassados at&eacute; o final da d&eacute;cada.</p> 
+</blockquote> 
+<blockquote> 
+ <p>A quest&atilde;o para um gerenciamento eficiente n&atilde;o &eacute; mais se os processos de sua organiza&ccedil;&atilde;o precisam ser melhorados, mas sim quais, quantos e quando.</p> 
+</blockquote> 
+<p>Uma das coisas que mudou de acordo com Brad Power &eacute; que a melhoria do processo precisa ser mais r&aacute;pida:</p> 
+<blockquote> 
+ <p>As empresas hoje precisam ser capazes de fazer mudan&ccedil;as em seus processos em dias ou semanas, e n&atilde;o em meses ou anos.</p> 
+</blockquote> 
+<p>Em uma <a href="http://blogs.hbr.org/cs/2012/12/in_my_last_post_i_1.html">continua&ccedil;&atilde;o</a>, tamb&eacute;m escrita por Brad Power, o autor descreve novas formas de como as organiza&ccedil;&otilde;es podem melhorar seus processos:</p> 
+<blockquote> 
+ <p>Estou me referindo coletivamente a essas abordagens de melhoria de processos como &quot;Estrat&eacute;gia de Processos 2.0&quot;. Elas est&atilde;o sobre os ombros dos m&eacute;todos da &quot;Estrat&eacute;gia de Processos 1.0&quot;: Lean, Six Sigma, e a Reengenharia de Neg&oacute;cios.</p> 
+</blockquote> 
+<p>Power afirma que uma abordagem &aacute;gil &eacute; necess&aacute;ria para satisfazer a crescente necessidade de velocidade na melhoria dos processos:</p> 
+<blockquote> 
+ <p>Para tornar mais r&aacute;pidas as opera&ccedil;&otilde;es e as melhorias, a Estrat&eacute;gia de Processos 2.0 far&aacute; um uso maior de experimentos r&aacute;pidos e processos de gest&atilde;o mais &aacute;geis.</p> 
+</blockquote> 
+<p>No site InfoWorld, Bob Lewis publicou o artigo <a href="http://www.infoworld.com/t/agile-development/adapt-agile-build-better-business-204332">Adapte-se ao Agile para construir um neg&oacute;cio melhor</a>, sugerindo como conseguir mudan&ccedil;as no neg&oacute;cio utilizando Scrum. Primeiro Lewis deixa claro que implementar mudan&ccedil;as em um neg&oacute;cio &eacute; diferente de entregar software:</p> 
+<blockquote> 
+ <p>Mudan&ccedil;as nos neg&oacute;cios, ent&atilde;o, envolveriam mais do que apenas lan&ccedil;ar uma nova parte de um software e esperar que todos descubram como us&aacute;-lo para melhorar o neg&oacute;cio. Essa mudan&ccedil;a acompanha tarefas adicionais, como por exemplo, planejar a melhoria dos processos e pr&aacute;ticas do neg&oacute;cio, realizar treinamentos, comunicar, fazer mudan&ccedil;as cont&aacute;beis, redefinir m&eacute;tricas de desempenho e muitas outras coisas.</p> 
+</blockquote> 
+<p>As hist&oacute;rias de usu&aacute;rio no backlog de produtos precisam cobrir tudo o que for necess&aacute;rio para fazer a mudan&ccedil;a nos neg&oacute;cios. Os itens do backlog ir&atilde;o &quot;descrever como o trabalho &eacute; feito usando o novo software&quot;, que na verdade &eacute; a inten&ccedil;&atilde;o da mudan&ccedil;a do neg&oacute;cio.</p> 
+<blockquote> 
+ <p>Quando chega a hora de trabalhar em uma hist&oacute;ria, aspectos que n&atilde;o t&ecirc;m nada a ver com o software ser&atilde;o atribu&iacute;dos a equipes de neg&oacute;cios com as qualifica&ccedil;&otilde;es apropriadas, que, al&eacute;m de trabalhar diretamente com o desenvolvedor, ter&atilde;o suas pr&oacute;prias responsabilidades. Por exemplo, &quot;Educar os usu&aacute;rios no novo processo e software.&quot; Isto entrar&aacute; no backlog, gerenciado da mesma forma que qualquer outra est&oacute;ria de usu&aacute;rio.</p> 
+</blockquote> 
+<p>Para definir as prioridades na mudan&ccedil;a do neg&oacute;cio, o autor sugere a utiliza&ccedil;&atilde;o da Teoria das Restri&ccedil;&otilde;es (TOC) de Goldratt:</p> 
+<blockquote> 
+ <p>A Teoria das Restri&ccedil;&otilde;es funciona por meio do estabelecimento de uma meta clara - por exemplo, reduzir o custo incremental, diminuir o tempo do ciclo, aumentar a capacidade de processamento. Em seguida, essa Teoria identifica a maior barreira existente para alcan&ccedil;ar a meta. Este &eacute; o gargalo - a restri&ccedil;&atilde;o. Livre-se da restri&ccedil;&atilde;o, e voc&ecirc; estar&aacute; mais perto de alcan&ccedil;ar o seu objetivo. Em seguida, identifique a maior restri&ccedil;&atilde;o restante, e livre-se dela tamb&eacute;m.</p> 
+</blockquote> 
+<p>De acordo com Lewis, usar Scrum combinado com a Teoria das Restri&ccedil;&otilde;es, para fazer mudan&ccedil;a no neg&oacute;cio, ajuda as organiza&ccedil;&otilde;es a tornarem-se empresas &aacute;geis.</p> 
+<blockquote> 
+ <p>O que torna uma empresa &aacute;gil &eacute; a capacidade de projetar, planejar e alcan&ccedil;ar uma mudan&ccedil;a intencional, de forma r&aacute;pida e confi&aacute;vel.</p> 
+</blockquote> 
+<p>Na publica&ccedil;&atilde;o <a href="http://agilemanagement.net/index.php/Blog/change_management_vs_process_evolution/">Gerenciamento de mudan&ccedil;as vs evolu&ccedil;&atilde;o de processos</a>, David Anderson descreve suas ideias sobre melhoria de processos de neg&oacute;cios, as quais chama de gerenciamento de mudan&ccedil;as:</p> 
+<blockquote> 
+ <p>Mudan&ccedil;as nos processos, ou mudan&ccedil;as na estrutura organizacional, est&atilde;o constituem a disciplina usada para trazer um pouco de controle e de governan&ccedil;a para estas atividades chamadas de gerenciamento de mudan&ccedil;as.</p> 
+</blockquote> 
+<p>David Anderson tamb&eacute;m explica porque a ado&ccedil;&atilde;o de Agile nas organiza&ccedil;&otilde;es, que tamb&eacute;m &eacute; uma gest&atilde;o de mudan&ccedil;a, &agrave;s vezes &eacute; um desafio:</p> 
+<blockquote> 
+ <p>&Eacute; ir&ocirc;nico que a abordagem para as transi&ccedil;&otilde;es para o Agile tenham sido muito n&atilde;o-&aacute;geis. Grandes projetos for&ccedil;am-nos a seguir um plano. O motivo de que muitas transi&ccedil;&otilde;es para o Agile s&atilde;o dif&iacute;ceis e pouco produtivas pode estar na abordagem utilizada para o dom&iacute;nio do problema. O que n&oacute;s precisamos &eacute; de uma abordagem &aacute;gil para mudar - uma abordagem que incorpore feedback e evolua conforme uma nova informa&ccedil;&atilde;o &eacute; apresentada.</p> 
+</blockquote> 
+<p>Assim como Bob Lewis, David Anderson tamb&eacute;m se refere &agrave; Teoria das Restri&ccedil;&otilde;es (ToC) de Goldratt. Anderson sugere aplicar as cinco etapas de foco da ToC como uma solu&ccedil;&atilde;o para o gerenciamento de mudan&ccedil;a evolutiva, concentrando-se na maior restri&ccedil;&atilde;o e eliminando-a:</p> 
+<blockquote> 
+ <p>Essencialmente, pequenas mudan&ccedil;as foram feitas e as sucessivas altera&ccedil;&otilde;es n&atilde;o poderiam ser previstas com muita anteced&ecirc;ncia - talvez s&oacute; as pr&oacute;ximas 2 ou 3 etapas, na melhor das hip&oacute;teses. Uma transi&ccedil;&atilde;o completa n&atilde;o poderia ser prevista.</p> 
+</blockquote> 
+<p>M&eacute;todos como <a href="http://www.infoq.com/br/minibooks/priming-kanban-jesper-boeg;jsessionid=6FF77A7F7D8F837046E5341A7FC724D3">Kanban</a> e Lean Startup podem ajudar a encontrar as maiores restri&ccedil;&otilde;es que impedem a melhoria na organiza&ccedil;&atilde;o:</p> 
+<blockquote> 
+ <p>O Kanban oferece &agrave;s empresas a capacidade de mudan&ccedil;a evolutiva. ... Com o Kanban usamos crit&eacute;rios de adequa&ccedil;&atilde;o ... para avaliar o processo atual e determinar como podemos guiar as mudan&ccedil;as para produzir uma evolu&ccedil;&atilde;o apta ao nosso processo.</p> 
+ <p>Da mesma forma, o Lean Startup surgiu como abordagem de Eric Ries a evolu&ccedil;&atilde;o do modelo de neg&oacute;cio. ... Com o Lean Startup os crit&eacute;rios de aptid&atilde;o s&atilde;o mais diretamente relacionadas com o mercado ... O conceito de valida&ccedil;&atilde;o de hip&oacute;teses de neg&oacute;cio, no Lean Startup, &eacute; o processo de aplicar um crit&eacute;rio de aptid&atilde;o num sentido evolucion&aacute;rio.</p> 
+</blockquote> 
+<p>Na publica&ccedil;&atilde;o <a href="http://www.softwareag.com/blog/reality_check/index.php/process-power/process-improvement/">Como as organiza&ccedil;&otilde;es alcan&ccedil;am a melhoria real em seu processos</a>, o chefe de BPM Empresarial da AG Software, Joerg Klueckmann, descreve as experi&ecirc;ncias de organiza&ccedil;&otilde;es com a melhoria de processos. Uma dessas organiza&ccedil;&otilde;es &eacute; a Cable &amp; Wireless:</p> 
+<blockquote> 
+ <p>A excel&ecirc;ncia nos processos est&aacute; na vis&atilde;o central da Cable &amp; Wireless. A empresa elevou o BPM de seu setor de TI a um n&iacute;vel de neg&oacute;cio mais estrat&eacute;gico na organiza&ccedil;&atilde;o, e compartilha suas experi&ecirc;ncias, que v&atilde;o desde o nascimento de um processo at&eacute; o alcance da excel&ecirc;ncia.</p> 
+ <p>Uma das melhores pr&aacute;ticas de melhoria de processo na empresa &eacute; seu processo Agile. O Agile &eacute; capaz de ajudar a mudar rapidamente e al&eacute;m disso, ter processos &aacute;geis n&atilde;o significa simplesmente Desenvolvimento &Aacute;gil em TI. Trata-se tamb&eacute;m de mudan&ccedil;as r&aacute;pidas e orientadas para o neg&oacute;cio.</p> 
+</blockquote> 
+<p id="lastElm"></p><br><br><br><br><br><br></body></html>
--- InfoQ/list.c
+++ InfoQ/list.c
@@ -1,6 +1,9 @@
 <html>
  <head></head> 
- <body> 
+ <body>
+  <div>
+   <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/34.c">Melhoria de processos de neg&oacute;cios usando Agile: discuss&otilde;es recentes</a>Organiza&ccedil;&otilde;es querem melhorar seus processos de neg&oacute;cios e nos dias de hoje precisam fazer essa melhoria de forma r&aacute;pida. &Eacute; poss&iacute;vel utilizar t&eacute;cnicas &aacute;geis para melhoria de processos de neg&oacute;cios?
+  </div> 
   <div> 
    <a href="https://raw.github.com/TomJerryA/Test/master/InfoQ/33.c">Backbone.js 1.0: primeira vers&atilde;o maior em 2 anos</a>Ap&oacute;s dois anos e meio de desenvolvimento, a vers&atilde;o 1.0 do Backbone.js foi lan&ccedil;ada. O Backbone.js &eacute; uma popular biblioteca JavaScript que segue o modelo Model/View, utilizada por v&aacute;rios aplicativos web populares, incluindo USA Today, Rdio e Airbnb. Veja o que mudou. 
   </div> 
