0a719f0ae7a8a7dcb91f662687375ecf05cfb338
Tom <tomjerrya@gmail.com>
2014-01-15 01:27:03 +0800

    update

--- InfoQ/10.java
+++ InfoQ/10.java
@@ -1,18 +1,66 @@
-<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body><h3>IntelliJ 13 Released</h3><p>JetBrains has announced the general release of <a href="http://www.jetbrains.com/idea/">IntelliJ IDEA version 13</a>. This release is in keeping with their traditional schedule of one major release per year, following version 12 which was released in December 2012. IntelliJ IDEA is the only one of the three popular IDE's (Eclipse, IntellJ, NetBeans) that offers a paid version.</p>
-<p>Sporting a completely revamped user interface, the IDE has updated its support for many development technologies introduced this year including Java EE 7, Glassfish 4, Tomcat 8, and others. It has also refined much of the existing functionality, especially Spring support, Gradle integration, and Android development tools.&nbsp;It also provides full support for Java 8, the largest update to the Java platform since Java 5, scheduled for release in March 2014. A full<a href="http://www.jetbrains.com/idea/whatsnew/"> list of new features</a> can be found on the JetBrains website.</p>
-<p>One important addition to the IDE is Android Studio, a new development environment built by Google on top of IntelliJ's free IDEA Community Edition in a non-commercial partnership.&nbsp;<br /> <br /> JetBrains has continued to work closely with Typesafe Inc, current stewards of Scala, to improve support for Scala. The new release features built-in integration for the SBT Scala build tool, and many other features.&nbsp;There is also a new dedicated Spring tool window.</p>
-<p>Besides the technology support, there are also many new usability enhancements, including the Search Everywhere feature that provides a single dialog for locating anything from classes to IDE settings, a smart find features that augments the standard case-sensitive, whole-words only and regular-expressions searches, with &quot;Comments only&quot; and &quot;String literals only&quot; selections. There is also a new &quot;Presentation Mode&quot; that allows you to record, edit, and annotate a session for use as a presentation or talk.</p>
-<p>InfoQ loaded-up the latest release of Ultimate Edition. Installation was fast, picking up the previous installation configuration, and the usage was as intuitive as ever. Support for Lambda expressions was apparent in several areas. First, refactorings for the new Collections API's are now offered as suggested refactorings. Additionally, even if you don't refactor to Lambdas, the code optionally folds to display the concise new Lambda syntax. One feature we'd like to see would be to suggest Lambda expressions as parameters.</p>
-<p>Depending on what you're doing, you may be able to get by with the Community Edition. The <a href="http://www.jetbrains.com/idea/features/editions_comparison_matrix.html">comparison matrix</a> on the JetBrains website lists the features that are exclusive to the Ultimate Edition, including:</p>
-<ul> 
- <li>UML Designer</li> 
- <li>Code Coverage tools</li> 
- <li>HTML, CSS, JavaScript, Struts</li> 
- <li>Spring Framework</li> 
- <li>JPA/Hibernate</li> 
- <li>Grails (Groovy is supported in Community Edition)</li> 
- <li>Application server support</li> 
- <li>Java EE 7 support</li> 
-</ul>
-<p>Community Edition is free. Ultimate Edition is listed at $199 for new individual users and $99 for upgrades. Both versions are available on Windows, Mac, and Linux, and can be downloaded from the <a href="http://www.jetbrains.com/idea/download/">IntelliJ IDEA download page</a>.</p>
-<p>&nbsp;</p><br><br><br><br><br><br></body></html>
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8" /></head><body><h3>Combining an Agile and Lean Approach</h3><p>Several approaches exist to&nbsp;improve software development, among them are agile and lean. Managers have to decide which approaches to deploy in their organization. Approaches can also be combined depending on the problems that need to be solved.</p>
+<p>In his talk <a href="http://www.lkce13.com/app/download/8238279795/Tue_Regis+Medina+-+Agile+and+Lean+-+LKCE13+-+v20131103.pdf?t=1383779635">agile for building, lean for learning</a> R&eacute;gis Medina shared his learnings of&nbsp;the two&nbsp;approaches. InfoQ interviewed him about combining agile and lean, focusing on people and learning.</p>
+<p><strong>InfoQ: Your presentation at the lean and kanban Central Europe Conference was about &quot;agile for building, lean for learning&quot;. Why do you recommend this combination? </strong></p>
+<blockquote> 
+ <p><strong>R&eacute;gis:</strong> 10 years after the Manifesto, agile has proved to be effective in reducing development cycles, drastically improving quality, eliminating silos within the teams, and making project management easier. However, when I consider the issues agile teams face on the field, I see three new challenges emerging:</p> 
+ <ul> 
+  <li><strong>Making the right products</strong>: that requires refining the product owner’s practices. I see too many product owners changing the course of action between iterations or asking for features that will end up not being used. The challenge is to develop the product owner’s skills beyond collecting users’ needs and demands in order to develop only the necessary features.</li> 
+  <li><strong>Delivering more at the end of each iteration</strong>. Too often, the number of features delivered frustrates the client because she has her own deadlines to meet, regardless of what was negotiated with the development team. In that case, agile teams are often clueless because they don’t know how to develop faster while preserving the quality of their software and not working long extra hours.</li> 
+  <li><strong>Learning how to better collaborate with the other departments</strong> within the company, up and down the value stream. I just mentioned the case of marketing teams requiring more features faster. At the other end of the stream we find production teams, for which promising opportunities are emerging with the rise of DevOps. But concretely, agile teams have a hard time taking others onboard, which often slows down the delay for production or causes incidents. One of the main obstacles I see in that area is the difficulty for agile teams to interact with their own management.</li> 
+ </ul> 
+ <p>On these topics, the agile community is constantly exploring and trying out new ideas. Personally, after 10 years practicing agile, I decided to step back a little and took the opportunity to learn lean in other areas of IT under the guidance of real lean sensei. When I saw really spectacular results in various domains, I realized that there was a huge opportunity here for the agile community.</p> 
+ <p>Lean and agile are perfectly compatible but very different in nature. Lean is not a process, or a toolbox for improving processes, but a management practice that individuals develop along the years with the help of their sensei. Managers and team leaders who practice lean actually learn:</p> 
+ <ul> 
+  <li>How to improve customer satisfaction, well beyond her initial demands.</li> 
+  <li>How to identify improvement opportunities in their teams in order to obtain significant and measurable performance gains, and link them to the company’s needs. They achieve this by getting things done right the first time, improving the flow of work across people, or finding ways to accomplish certain tasks much faster.</li> 
+  <li>How to create a working environment in which team members develop the right skills much faster and think deeper about how to perform their job better.</li> 
+ </ul> 
+</blockquote>
+<p><strong>InfoQ: How does the combination of agile and lean help us to increase our insights in the value that we can deliver to customers? </strong></p>
+<blockquote> 
+ <p><strong>R&eacute;gis: </strong>Because of its iterative nature, agile does involve the client - or at least a client representative - a lot in the project. But there is room for improvement when it comes to understanding value from the client perspective. This is not easy, since what the client asks for is not necessarily what she really needs or what will drive her buying decision afterwards.</p> 
+ <p>This is where lean comes in handy:</p> 
+ <ul> 
+  <li>It develops a “go &amp; see” habit. In lean this is called “genchi genbutsu”, it means going to the field in order to understand the client context and to formulate the problem he is trying to solve.</li> 
+  <li>It provides a framework to analyze the client’s expectations and understand the way he uses the product or service. Lean develops your ability to see value as well as waste on the field. This helps understand the customer’s experience and improve the product so that it creates as little waste as possible for its users.</li> 
+  <li>It helps align the project members on these objectives, which are more focused on the problems to solve for the customer than on the list of features to develop.</li> 
+  <li>It makes you see the product evolution as a constant flow of experimentations that helps better understand the clients’ context and preferences.</li> 
+ </ul> 
+</blockquote>
+<p><strong>InfoQ: Both agile and lean help us to pay attention to people. Can you explain how they compliment each other. </strong></p>
+<blockquote> 
+ <p><strong>R&eacute;gis: </strong>Agile strengthens the collaboration and communication between the stakeholders of the project, and creates and environment that helps the teams do a better job at an acceptable pace. This aspect is very close to lean’s “Respect for people”. But lean goes a lot further with the notion of training.</p> 
+ <p>Taiichi Ohno believed that the waste we see every day in our organizations is a result of our misconceptions. In lean, investing in skills development and improving everyone’s capacity of judgment is the key to success. The main lean question being: “who needs to learn what for the company to succeed?”&nbsp;</p> 
+ <p>Indeed, lean is a set of practices and exercises aimed at developing people skills:</p> 
+ <ul> 
+  <li>The problem solving approach based on the &quot;Plan-Do-Check-Act&quot; cycle helps clarify one’s thinking in order to, first, choose the right problems, and second, take decisions based on facts rather than on beliefs.</li> 
+  <li>The two pillars of the Toyota Production System - Just-in-Time and Jidoka - define standard types of problems that people need to solve constantly to improve their skills: work together at the right pace and do everything right the first time.</li> 
+  <li>Writing standards and performing continuous on the job training helps accelerate skills development and spread the know-how, be it at team level or companywide.</li> 
+ </ul> 
+ <p>These practices perfectly fit in the agile world.</p> 
+</blockquote>
+<p><strong>InfoQ: Should an organization start with agile, and then adopt lean, or the other way around. Or are there ways to mix both approaches and do them at the same time? </strong></p>
+<blockquote> 
+ <p><strong>R&eacute;gis: </strong>The lean answer to that question is actually another question.&nbsp;</p> 
+ <p>As a set of development practices, agile is a solution. The lean question is: what is the problem you are facing for which agile could be a solution? Imposing a solution when no problem exists could lead to a waste of time, create more problems than it solves, or leave the real problem untouched.</p> 
+ <p>Following that logic, I would encourage a manager asking these questions to:</p> 
+ <ol> 
+  <li>Clearly define the problem she needs to solve, and clarify the reason why this problem is important for the company.</li> 
+  <li>Engage all team members in the challenge.</li> 
+  <li>Identify with them, on the field, the main obstacles that get in their way on their path to success.</li> 
+  <li>Lead experimentations that will help them overcome these specific obstacles.</li> 
+  <li>pread and share the know-how within the team and beyond, across the whole organization.</li> 
+ </ol> 
+ <p>...which actually means starting with lean!</p> 
+</blockquote>
+<p><strong>InfoQ: You mentioned lean for learning. What about agile retrospectives? They can also help teams to reflect and learn. </strong></p>
+<blockquote> 
+ <p><strong>R&eacute;gis: </strong>Yes, agile retrospective are perfect opportunities to learn, even though the results tend to vary widely depending on the skills of the coach who is leading them. To me, the retrospective will be efficient and useful if you ask the following questions:</p> 
+ <ul> 
+  <li>How does the team know it is working on the right problems and learning what it needs to learn, rather than what it wants to learn?</li> 
+  <li>How does that learning improve the skills of each and every member of the team?</li> 
+  <li>How do you evaluate the benefits of this learning for the customers and for the company?</li> 
+ </ul> 
+ <p>That being said, retrospectives are more a batch approach to improvement, happening once every iteration and tackling (hopefully) a limited number of problems. Lean helps develop kaizen on a daily basis, one problem after the other, to make sure that everyone, every day, contributes to the improvement. It is based on a different approach to visual management, the Obeya model, that aims at making problems visible to drive the team to constantly collaborate in order to solve these problems. A kind of “continuous retrospective”, I should say.</p> 
+</blockquote>
+<p>You can view the video of the talk <a href="http://www.youtube.com/watch?v=kezSFG2Pslk&amp;feature=youtu.be">Lean &amp; Agile by R&eacute;gis Medina</a>&nbsp;from the Lean IT Summit 2013.</p><br><br><br><br><br><br></body></html>
